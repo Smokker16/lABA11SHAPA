d26ae108c88694b59647c6c1f127e9509479655e
weenu05 <nikiton151006@gmail.com>
2025-04-15 09:34:16 +0300

    commit cod

---
+++ LABA11SHAPA/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABA11SHAPA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double angleInRadians = Math.Atan(2);// Вычислим арктангенс от числа 2
+            Console.WriteLine("Общее решение уравнения sin(x) - 2*cos(x) = 0:");
+            Console.WriteLine($"x = {angleInRadians:F4} + n*π, где n — целое число");
+            Console.WriteLine("Работу выполнил Шапкин");
+            
+        }
+    }
+}
